c2b54663cf72a73cdbd54d09c5daac15130eebf3
keyohh <132158961+keyohhh@users.noreply.github.com>
2024-07-18 13:36:44 -0400

    save state

---
+++ script.js
@@ -0,0 +1,8 @@
+const textArea = document.querySelector('textarea')
+
+textArea.addEventListener('input', autoResize, false)
+
+function autoResize() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px'
+}
