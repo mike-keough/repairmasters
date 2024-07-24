0d0672a729d921eada93ad19b0fc3ad1fc469ff9
keyohh <132158961+keyohhh@users.noreply.github.com>
2024-07-24 11:59:45 -0400

    removed feature

--- script.js
+++ script.js
@@ -9,15 +9,3 @@ function autoResize() {
 
 let phoneNumber = document.getElementById('phone-number')
 
-function validatePhone(phone) {
-    var phoneRegex = /^\(\d{3}\)\s\d{3}-\d{4}$/;
-    return phoneRegex.test(phone);
-  }
-  
-phoneNumber.addEventListener('input', function(e) {
-    var phone = document.getElementById('phone-number').value;
-    if (!validatePhone(phone)) {
-        e.preventDefault();
-        alert('Please enter a valid phone number in the format (123) 456-7890');
-    }
-});
