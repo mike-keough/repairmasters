170458c2602f78d8e1697e158556bd5b405327d5
keyohh <132158961+keyohhh@users.noreply.github.com>
2024-07-24 11:44:17 -0400

    test

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function autoResize() {
     this.style.height = 'auto';
     this.style.height = this.scrollHeight + 'px'
 }
+
+let phoneNumber = document.getElementById('phone-number')
+
+function validatePhone(phone) {
+    var phoneRegex = /^\(\d{3}\)\s\d{3}-\d{4}$/;
+    return phoneRegex.test(phone);
+  }
+  
+phoneNumber.addEventListener('input', function(e) {
+    var phone = document.getElementById('phone-number').value;
+    if (!validatePhone(phone)) {
+        e.preventDefault();
+        alert('Please enter a valid phone number in the format (123) 456-7890');
+    }
+});
